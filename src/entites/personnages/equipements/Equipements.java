8af54f6ffc06d60a2199bbbb52235f73b48875f2
Lohan Marchand <lohan.marchand@etu.unistra.fr>
2025-05-07 12:10:24 +0200

    Codage de la classe Armes

--- src/entites/personnages/equipements/Equipements.java
+++ src/entites/personnages/equipements/Equipements.java
@@ -1,4 +1,18 @@
 package entites.personnages.equipements;
 
-public class Equipements {
+public abstract class Equipements {
+    private String m_nom;
+    public Equipements(String nom) {
+        m_nom = nom;
+    }
+    public Equipements() {
+        m_nom = "Equipement sans nom";
+    }
+    public String getNom() {
+        return m_nom;
+    }
+    @Override
+    public String toString() {
+        return m_nom;
+    }
 }

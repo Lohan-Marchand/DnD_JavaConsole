31e358159f299a1ebf7bf89f05bb99f70c5efb47
Lohan Marchand <lohan.marchand@etu.unistra.fr>
2025-05-31 19:52:01 +0200

    Modification de Guerison.java

--- src/entites/personnages/sorts/Guerison.java
+++ src/entites/personnages/sorts/Guerison.java
@@ -3,15 +3,23 @@ package entites.personnages.sorts;
 import dice.D10;
 import entites.personnages.Personnages;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Guerison extends Sorts{
     public Guerison(){
         super("Guérison", "Soigne un personnage de 1d10 PVs.");
     }
 
     public void lancerSort(Personnages personnage) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lancez 1d10 pour le nombre de PV à guérir  (appuyez sur entrer)");
+        sc.nextLine();
         int soins = new D10().rollDice();
+        System.out.print("Vous avez fait " + soins);
+
         personnage.setPV(personnage.getPV() + soins);
-        System.out.println(personnage.getPseudo()+" a été soigné de "+soins+" PVs.");
+        System.out.println(personnage.getPseudo()+" a été soigné de "+soins+" PVs. ("+personnage.getPV() +"/"+personnage.getPVMax()+")");
     }
     @Override
     public void lancerSort(){

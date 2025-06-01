1b0740b91ae89bfe09effde2fd0048541c19cbbb
Lohan Marchand <lohan.marchand@etu.unistra.fr>
2025-06-01 18:02:32 +0200

    correction inventaire guerriers

--- src/entites/personnages/classes/Guerriers.java
+++ src/entites/personnages/classes/Guerriers.java
@@ -1,12 +1,15 @@
 package entites.personnages.classes;
 
+import entites.personnages.equipements.armes.deGuerre.EpeeLongue;
 import entites.personnages.equipements.armes.distance.ArbaleteLegere;
 import entites.personnages.equipements.armures.legeres.ArmureEcaille;
+import entites.personnages.equipements.armures.lourdes.CoteMaille;
 
 public class Guerriers extends Classes{
     public Guerriers(){
         super("Guerrier", 20);
         setInventaire(new ArbaleteLegere());
-        setInventaire(new ArmureEcaille());
+        setInventaire(new CoteMaille());
+        setInventaire(new EpeeLongue());
     }
 }
